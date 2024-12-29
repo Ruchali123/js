375c3e123aa92caf39a3bd00d156836ec09c5b71
Ruchali Babulkar <ruchali12babulkar@gmail.com>
2024-12-29 17:01:05 +0530

    Conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,34 @@
+let score = true
+//let score = "33"
+//let score = "33abc"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+//run: node flodername/filename;
+
+//"33" => 33
+//"33abc" => NaN (Not in a Number)
+//true => 1; false=> 0
+
+let isLoggedIn = "rucha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+//"" => false
+//"rucha" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
