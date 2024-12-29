f056853f108aad7dfd05125f516c49c4523dd899
Ruchali Babulkar <ruchali12babulkar@gmail.com>
2024-12-29 22:02:23 +0530

    Comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("2" >= 1);
+
+//-----------------------------------------------------
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//at time of comparision null is converted into the zero
+
+// avoid this type of comparision
+
+// console.log(undefined == 0);
+
+//===
+
+// console.log("2" === 2);
+//-----------------------------------------------------
