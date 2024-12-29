368e1afa6f17932bd1734d00a8521b99e361a035
Ruchali Babulkar <ruchali12babulkar@gmail.com>
2024-12-29 20:40:31 +0530

    Conversion of Operation 2

---
+++ 01_basics/conversionOperation2.js
@@ -0,0 +1,44 @@
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "rucha"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// // gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log("x:" + x, "y:" + y);
+
+let a= 2;
+const b = ++a;
+console.log("a:" + a, "b:" + b);
+
