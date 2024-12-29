c7bc5c077630f11456be8146ac17de741106c1e6
Ruchali Babulkar <ruchali12babulkar@gmail.com>
2024-12-29 16:30:51 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // use all code as a newer verision of code
+
+//alert() //we are useing nodejs not browser
+
+console.log(3+3) 
+// code readability should be high
+console.log('rucha') 
+
+let name = "Rucha"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//primitive datatypes
+//number => 2 to power 53
+//bigint use in strong wedsite
+//string => ""
+//boolean => true/false
+//null => it is stand alone value 
+//undefined => variable declare but value is not given 
+//symbol => to find uniqueness we use symbol
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountID = 123
+let accountName = "rucha"
+var accountPassword ="1256"
+accountCity = "Nagpur"
+
+console.table([accountID,accountName,accountPassword,accountCity]);
+
